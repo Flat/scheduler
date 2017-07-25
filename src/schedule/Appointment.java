cf79d1c1884b86b5976c271b2a8500ed0276f113
Ken Swenson <flat@imo.uto.moe>
2017-07-24 20:54:28 -0400

    getter and setter

--- src/schedule/Appointment.java
+++ src/schedule/Appointment.java
@@ -17,4 +17,125 @@ public class Appointment {
     private String createdBy;
     private Timestamp lastUpdate;
     private String lastUpdateBy;
+
+    public int getAppointmentid() {
+        return appointmentid;
+    }
+
+    public void setAppointmentid(int appointmentid) {
+        this.appointmentid = appointmentid;
+    }
+
+    public int getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescritpion() {
+        return descritpion;
+    }
+
+    public void setDescritpion(String descritpion) {
+        this.descritpion = descritpion;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
+    public void setLastUpdateBy(String lastUpdateBy) {
+        this.lastUpdateBy = lastUpdateBy;
+    }
+
+    public Appointment(int appointmentid, int customerid, String title, String descritpion, String location, String contact, String url, Date start, Date end, Date createDate, String createdBy, Timestamp lastUpdate, String lastUpdateBy) {
+
+        this.appointmentid = appointmentid;
+        this.customerid = customerid;
+        this.title = title;
+        this.descritpion = descritpion;
+        this.location = location;
+        this.contact = contact;
+        this.url = url;
+        this.start = start;
+        this.end = end;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdate = lastUpdate;
+        this.lastUpdateBy = lastUpdateBy;
+    }
 }
